5024255a6923689dd85ea6e47bcc5001797c14c5
Joshua Oyelalu <47543718+Joshuaoyelalu@users.noreply.github.com>
2024-02-21 01:52:42 +0100

    main

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -139,3 +139,11 @@ export function unregister() {
       });
   }
 }
+
+
+fake_users = [
+  { "name": "User 1", "id": "user1", "city": "San Francisco", "state": "CA" },
+  { "name": "User 2", "id": "user2", "city": "New York", "state": "NY" },
+  { "name": "User 3", "id": "user3", "city": "Chicago", "state": "IL" }, 
+  { "name": "User 4", "id": "user4", "city": "Los Angeles", "state": "CA" },
+]
